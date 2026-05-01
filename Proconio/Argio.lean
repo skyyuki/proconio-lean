10d9bd8c8d0678793b2353aa235e68020ec48f8f
skyyuki <72378357+skyyuki@users.noreply.github.com>
2026-05-01 22:50:29 +0900

    feat: :sparkles: Add argio! utitity
    Add argio! elab syntax and Arg marker.
    argio! will parse function arguments and applies values generated by Readable.read.
    Arg serves as marker for convertion marker in a function used on argio!.

---
+++ Proconio/Argio.lean
@@ -0,0 +1,50 @@
+import Lean
+
+import Proconio.Basic
+
+
+open Lean Meta Elab Term
+
+
+@[reducible]
+def Arg (α : Type) [Readable α β] := β
+instance (priority := 900) [Readable α β] : Readable (Arg α) β where
+  read source := Readable.read α source
+
+
+syntax (name := argioE) "argio!" term "from" term : term
+
+@[term_elab argioE]
+def elabArgioE : TermElab := fun stx _ => do
+  let `(argio! $fn from $source) := stx | throwUnsupportedSyntax
+
+  let fn ← elabTerm fn none
+  let fnType ← inferType fn
+  let source ← elabTerm source none
+
+  forallTelescope fnType fun args _ => do
+    let mut currentExpr := fn
+    let mut argActions : Array (Expr × Expr) := #[] -- (arg, readAction)
+
+    for arg in args do
+      let argType ← inferType arg
+
+      let innerType := if argType.isAppOf `Arg then argType.getAppArgs[1]! else argType
+
+      let inst ← synthInstance (mkAppN (mkConst ``Readable) #[innerType, (← mkFreshExprMVar none)])
+
+      let readAction ← mkAppOptM
+        ``Readable.read
+        #[innerType, none, inst, none, none, source]
+
+      currentExpr := mkApp currentExpr arg
+      argActions := argActions.push (arg, readAction)
+
+    for (arg, readAction) in argActions.reverse do
+      currentExpr ← mkAppOptM
+        ``Bind.bind
+        #[mkConst ``IO, none, none, none, readAction, ← mkLambdaFVars #[arg] currentExpr]
+
+
+    return currentExpr
+
