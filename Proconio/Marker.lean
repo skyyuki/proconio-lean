4b98e9d29bbee7fc184953a79b09bd5b4f24f936
skyyuki <72378357+skyyuki@users.noreply.github.com>
2026-05-14 00:43:17 +0900

    feat: :label: Add some marker abbrevs

--- Proconio/Marker.lean
+++ Proconio/Marker.lean
@@ -15,3 +15,15 @@ instance {α β γ : Type} [Readable α β] {f : β → γ} : Readable (FromFunc
 For 1-indexed index input, interpreted as Nat - 1.
 -/
 abbrev Nat_1 : Type := FromFunc Nat (· - 1)
+/--
+Convert String into `List Char`
+-/
+abbrev Chars : Type := FromFunc String String.toList
+/--
+String into `ByteArray`
+-/
+abbrev Bytes : Type := FromFunc String String.toByteArray
+/--
+String into `List UInt8`
+-/
+abbrev BytesList : Type := FromFunc Bytes ByteArray.toList
